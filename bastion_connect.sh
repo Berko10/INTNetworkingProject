4bbed47445a70baf84c359dfd4f4d2a370f399a6
Berko10 <barberko1@gmail.com>
2024-08-05 14:54:59 +0300

    solution and bastion_connect

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -13,12 +13,9 @@ COMMAND="$3"
 if [ $# -lt 1 ]; then
 	echo "Please provide bastion IP address"
 elif [ -n "$COMMAND" ]; then
-  # Connect to the public instance, then the private instance, and run the command
   ssh -i "$KEY" "$EC2_USER@$PUBLIC_INSTANCE_IP" "ssh -i "$KEY" "$EC2_USER@$PRIVATE_INSTANCE_IP" "$COMMAND""
 elif [ -n "$PRIVATE_INSTANCE_IP" ]; then
-  # Connect to the public instance and then to the private instance
   ssh -t -i "$KEY" "$EC2_USER@$PUBLIC_INSTANCE_IP" "ssh -q -i "$KEY" "$EC2_USER@$PRIVATE_INSTANCE_IP""
 else
-  # Connect to the public instance
   ssh -q -t -i "$KEY" "$EC2_USER@$PUBLIC_INSTANCE_IP"
 fi
